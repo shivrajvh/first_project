91011b1ccbd807bd5fafac814a1246e7baccc5e5
Shivraj Hiremath <shivrajh@iquadra.com>
2018-04-06 17:41:58 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,24 @@
+#def my_function(name="Unknown", age="Unknown"):
+#    print("My name is", name, "and my age is", age)
+
+#my_function(age='30',name="Shiv")
+
+#def test(int1, int2):
+ #   a = int1+int2
+#    print ("Total is:", +a)
+
+#test(5,5)
+
+#def people_list(*person):
+#    for people in person:
+#        print("The name of this person is ", people)
+
+#people_list("Shivraj", "Rakesh", "Chetan", "Pratik")
+
+def addition(n1, n2):
+    return n1+n2
+
+p1 = addition(1,3)
+p2 = addition(10,11)
+
+print("first number is:", p1, "second number is:", p2)
