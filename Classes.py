211601e3f96aa8bbb1fc1f81771d0b7356c58488
Shivraj Hiremath <shivrajh@iquadra.com>
2018-04-09 17:38:35 +0530

    Commit for 09 april 2018 2 pyton files 1. Calculator 2. Loops

---
+++ Classes.py
@@ -0,0 +1,15 @@
+import random
+
+
+class Enemy:
+
+	def __init__(self, atkl, atkh):
+		self.atkl=atkl
+		self.atkl=atkh
+
+
+enemy1 = Enemy(40,50)
+enemy1.getAtk()
+
+enemy2 = Enemy(60,70)
+enemy2.getAtk()
